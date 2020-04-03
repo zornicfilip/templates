f463ecff1355a23740b9fd5a2653d3679806bdc4
zornicfilip <zornic.filip@gmail.com>
2020-04-03 21:34:12 +0200

    Dropdwon menu

--- git/general/dev/js/navigation.js
+++ git/general/dev/js/navigation.js
@@ -23,3 +23,22 @@ function theme2() {
     document.getElementsByTagName("body")[0].className = '';
     document.getElementsByTagName("body")[0].classList.add("theme2");
 }
+
+//Dropdown
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
