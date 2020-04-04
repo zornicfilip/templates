9b83e06ed29e6d329c343039a8565f1143f6b20e
zornicfilip <zornic.filip@gmail.com>
2020-04-04 20:40:46 +0200

    Camping css

--- git/general/dev/js/navigation.js
+++ git/general/dev/js/navigation.js
@@ -26,12 +26,12 @@ function theme2() {
 
 //Dropdown
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.querySelector(".dropdown div").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.dropdown-button')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
